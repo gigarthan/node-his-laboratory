309791cd04f9d73fef251c811774fa332e9b30f6
Neminda <nemindawarnaja@gmail.com>
2018-06-11 20:25:10 +0530

    Setup labType models

---
+++ src/models/labType.js
@@ -0,0 +1,23 @@
+//IT16139640
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const labTypeSchema = new Schema({
+
+
+    name: {
+        type: String,
+        required: true,
+        unique :true
+    }
+
+});
+
+module.exports = mongoose.model('LabType', labTypeSchema);
+
+
+
+
+
+
