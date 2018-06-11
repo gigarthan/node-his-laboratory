27524d7b7063e294d13d63f3357b579dce4dcd92
Neminda <nemindawarnaja@gmail.com>
2018-06-11 11:46:34 +0530

    Setup testCategory models

---
+++ src/models/testCategory.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const testCategoriesSchema = new Schema({
+
+    name:{
+
+        type:String,
+        required :true
+    },
+
+    subCategories:{
+
+        type :Array
+    },
+
+});
+
+module.exports = mongoose.model('TestCategories',testCategoriesSchema);
