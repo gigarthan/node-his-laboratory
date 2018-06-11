2528e58c3eb4ec72f036e4f29442e9314cfa8bde
Neminda <nemindawarnaja@gmail.com>
2018-06-11 20:20:26 +0530

    Setup sampleCenterType models

---
+++ src/models/sampleCenterType.js
@@ -0,0 +1,18 @@
+//IT16139640
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const sampleCenterTypeSchema = new Schema({
+
+
+    name: {
+        type: String,
+        required: true,
+        unique :true
+    }
+
+});
+
+module.exports = mongoose.model('SampleCenterType', sampleCenterTypeSchema);
+
