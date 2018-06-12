5d3c61f1dca4857b2eb2ed819dd04d9969e67669
Neminda <nemindawarnaja@gmail.com>
2018-06-12 09:56:03 +0530

    Setup test category routes

--- src/routes/testCategoryRouter.js
+++ src/routes/testCategoryRouter.js
@@ -3,10 +3,77 @@ const router = express.Router();
 
 const testCategoryController = require('../controllers/testCategoryController');
 
-router.get('/', testCategoryController.getAllTestCategory);
 
-router.post('/addTestCategory', testCategoryController.addTestCategory);
 
-router.put('/updateTestCategory', testCategoryController.addTestCategory);
+//Get lab categories
+
+
+
+router.get('/',  (req, res)=> {
+    testCategoryController.getAllTestCategory().then(data=> {
+        res.status(data.status).send(data.data);
+    }).catch(err=> {
+        res.status(err.status).send(err.message);
+    });
+});
+
+
+
+//Get single test category
+
+
+
+router.get('/:name', (req, res) =>{
+    testCategoryController.getSingleTestCategory(req.params.name).then(data => {
+        res.status(data.status).send(data.data);
+    }).catch(err =>{
+        res.status(err.status).send(err.message);
+    });
+});
+
+
+
+//Add new test category
+
+
+
+router.post('/', (req, res) =>{
+    testCategoryController.addTestCategory(req.body).then(data => {
+        res.status(data.status).send(data.message);
+    }).catch(function (err) {
+        res.status(err.status).send(err.message);
+    });
+});
+
+
+
+//delete test category
+
+
+
+router.delete('/:name', (req, res) =>{
+    testCategoryController.deleteTestCategory(req.params.name).then(data =>{
+        res.status(data.status).send(data.response);
+    }).catch(err =>{
+        res.status(err.status).send(err.message);
+    });
+});
+
+
+
+//update test category
+
+
+
+router.put('/:name', (req, res)=> {
+    testCategoryController.updateTestCategory(req.params.name, req.body).then(data => {
+        res.status(data.status).send(data.message);
+    }).catch(err => {
+        res.status(err.status).send(err.message);
+    });
+});
+
+
+
 
 module.exports = router;
