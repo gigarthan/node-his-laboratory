2c4e05fe52929e5d7a4a526fb9fe90e9d62dbd0b
Erandi-deSilva <desilvaerandi.eds@gmail.com>
2018-06-12 07:13:22 +0530

    new controller file added.

---
+++ src/controllers/orderRequestController.js
@@ -0,0 +1,72 @@
+var mongoose = require('../config/database')
+var orderSchema = require('../models/order');
+
+var orderController = function () {
+
+    this.createNewOrder = function(orderData){
+        return new Promise((resolve,reject) => {
+            var orderRequest = new orderSchema({
+                reqId:orderData.reqId,
+                patientHIN:orderData.patientHIN,
+                fullName:orderData.fullName,
+                gender:orderData.gender,
+                dob:orderData.dob,
+                status:orderData.status,
+                priority:orderData.priority,
+                reqDate:orderData.reqDate,
+                dueDate:orderData.dueDate,
+                reqPerson:orderData.reqPerson,
+                comment:orderData.comment,
+                department:orderData.department,
+                category:orderData.category,
+                subCategory:orderData.subCategory,
+                testName:orderData.testName,
+            })
+            orderRequest.save().then(() => {
+                resolve({'status':201, 'message':'New Order Request Created!'});
+        }).catch(err => {
+                reject({'status':500, 'message':err});
+        })
+        })
+    }
+
+
+    this.viewAllOrderRequsts = function(){
+        return new Promise ((resolve,reject) => {
+            orderSchema.find().exec().then(data => {
+                resolve({'status':201, 'message':data});
+        }).catch(err => {
+                reject({'status':404, 'message':err});
+        })
+        })
+    }
+
+
+    this.viewOrderByReqId = function(reqId){
+        return new Promise ((resolve,reject) => {
+            orderSchema.find({reqId:reqId}).exec().then(data => {
+            resolve({'status':201, 'message':data});
+        }).catch(err => {
+            reject({'status':404, 'message':err});
+        })
+        })
+    }
+
+
+    this.viewOrderByPatientHIN = function(patientHIN){
+        return new Promise ((resolve,reject) => {
+            orderSchema.find({patientHIN:patientHIN}).exec().then(data => {
+            resolve({'status':201, 'message':data});
+        }).catch(err => {
+            reject({'status':404, 'message':err});
+        })
+        })
+    }
+
+
+
+
+
+}
+
+module.exports = new orderController();
