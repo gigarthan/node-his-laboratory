8bd47f0cb62ff30cb6ac4376e8439a005ae3bae8
Neminda <nemindawarnaja@gmail.com>
2018-06-13 21:30:21 +0530

    combining test category and test sub category schemas

--- src/models/labTest/testCategory.js
+++ src/models/labTest/testCategory.js
@@ -6,11 +6,27 @@ const Schema = mongoose.Schema;
 
 const testCategorySchema = new Schema({
 
-    name: {
-        type: String,
+    name:{
+        type:String,
         unique: true,
-        required: true
+        required :true
+    },
+    subCategoryName:{
+        type:String,
+       // unique: true,
+        required :true
+    },
+    specimenType: {
+        type: String,
+    },
+    specimenRetentionType: {
+        type: String,
+    },
+    duration: {
+        type: String,
     }
+
+
 
 });
 
