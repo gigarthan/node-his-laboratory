9636f035505f03651a04419f96828420a13539cc
Neminda <nemindawarnaja@gmail.com>
2018-06-15 21:26:05 +0530

    refactoring

--- src/models/sampleCenter/sampleCenterType.js
+++ src/models/sampleCenter/sampleCenterType.js
@@ -4,14 +4,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const sampleCenterTypeSchema = new Schema({
-
-
     name: {
         type: String,
         required: true,
         unique :true
     }
-
 });
 
 module.exports = mongoose.model('SampleCenterType', sampleCenterTypeSchema);
